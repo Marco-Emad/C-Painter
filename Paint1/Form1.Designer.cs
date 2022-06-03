dffbcbcee3f226bc08fe5d2a45c61dfde765f070
Marco Emad <56565607+Marco-Emad@users.noreply.github.com>
2022-06-03 02:21:09 +0200

    Add files via upload

---
+++ Paint1/Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿
+namespace Paint1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.pictureBox10 = new System.Windows.Forms.PictureBox();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.pictureBox11 = new System.Windows.Forms.PictureBox();
+			this.pictureBox12 = new System.Windows.Forms.PictureBox();
+			this.pictureBox8 = new System.Windows.Forms.PictureBox();
+			this.pictureBox7 = new System.Windows.Forms.PictureBox();
+			this.pictureBox6 = new System.Windows.Forms.PictureBox();
+			this.pictureBox5 = new System.Windows.Forms.PictureBox();
+			this.pictureBox4 = new System.Windows.Forms.PictureBox();
+			this.pictureBox3 = new System.Windows.Forms.PictureBox();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.Clearbtn = new System.Windows.Forms.Button();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.pictureBox9 = new System.Windows.Forms.PictureBox();
+			this.label2 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+			this.panel2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.White;
+			this.panel1.Location = new System.Drawing.Point(2, 73);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(1259, 566);
+			this.panel1.TabIndex = 0;
+			this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+			this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+			this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+			// 
+			// pictureBox10
+			// 
+			this.pictureBox10.ErrorImage = null;
+			this.pictureBox10.Image = global::Paint1.Properties.Resources._38402_200;
+			this.pictureBox10.Location = new System.Drawing.Point(667, 19);
+			this.pictureBox10.Name = "pictureBox10";
+			this.pictureBox10.Size = new System.Drawing.Size(46, 48);
+			this.pictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox10.TabIndex = 11;
+			this.pictureBox10.TabStop = false;
+			this.pictureBox10.Click += new System.EventHandler(this.pictureBox10_Click);
+			// 
+			// panel2
+			// 
+			this.panel2.BackColor = System.Drawing.Color.MistyRose;
+			this.panel2.Controls.Add(this.pictureBox11);
+			this.panel2.Controls.Add(this.pictureBox12);
+			this.panel2.Controls.Add(this.pictureBox8);
+			this.panel2.Controls.Add(this.pictureBox7);
+			this.panel2.Controls.Add(this.pictureBox6);
+			this.panel2.Controls.Add(this.pictureBox5);
+			this.panel2.Controls.Add(this.pictureBox4);
+			this.panel2.Controls.Add(this.pictureBox3);
+			this.panel2.Controls.Add(this.pictureBox2);
+			this.panel2.Controls.Add(this.pictureBox1);
+			this.panel2.Location = new System.Drawing.Point(1066, 2);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(185, 65);
+			this.panel2.TabIndex = 1;
+			// 
+			// pictureBox11
+			// 
+			this.pictureBox11.BackColor = System.Drawing.Color.Gray;
+			this.pictureBox11.Location = new System.Drawing.Point(147, 35);
+			this.pictureBox11.Name = "pictureBox11";
+			this.pictureBox11.Size = new System.Drawing.Size(30, 27);
+			this.pictureBox11.TabIndex = 1;
+			this.pictureBox11.TabStop = false;
+			this.pictureBox11.Click += new System.EventHandler(this.pictureBox1_Click);
+			// 
+			// pictureBox12
+			// 
+			this.pictureBox12.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			this.pictureBox12.Location = new System.Drawing.Point(147, 3);
+			this.pictureBox12.Name = "pictureBox12";
+			this.pictureBox12.Size = new System.Drawing.Size(30, 29);
+			this.pictureBox12.TabIndex = 2;
+			this.pictureBox12.TabStop = false;
+			this.pictureBox12.Click += new System.EventHandler(this.pictureBox1_Click);
+			// 
+			// pictureBox8
+			// 
+			this.pictureBox8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+			this.pictureBox8.Location = new System.Drawing.Point(111, 35);
+			this.pictureBox8.Name = "pictureBox8";
+			this.pictureBox8.Size = new System.Drawing.Size(30, 27);
+			this.pictureBox8.TabIndex = 7;
+			this.pictureBox8.TabStop = false;
+			this.pictureBox8.Click += new System.EventHandler(this.pictureBox1_Click);
+			// 
+			// pictureBox7
+			// 
+			this.pictureBox7.BackColor = System.Drawing.Color.Yellow;
+			this.pictureBox7.Location = new System.Drawing.Point(75, 35);
+			this.pictureBox7.Name = "pictureBox7";
+			this.pictureBox7.Size = new System.Drawing.Size(30, 27);
+			this.pictureBox7.TabIndex = 6;
+			this.pictureBox7.TabStop = false;
+			this.pictureBox7.Click += new System.EventHandler(this.pictureBox1_Click);
+			// 
+			// pictureBox6
+			// 
+			this.pictureBox6.BackColor = System.Drawing.Color.Aqua;
+			this.pictureBox6.Location = new System.Drawing.Point(39, 35);
+			this.pictureBox6.Name = "pictureBox6";
+			this.pictureBox6.Size = new System.Drawing.Size(30, 27);
+			this.pictureBox6.TabIndex = 5;
+			this.pictureBox6.TabStop = false;
+			this.pictureBox6.Click += new System.EventHandler(this.pictureBox1_Click);
+			// 
+			// pictureBox5
+			// 
+			this.pictureBox5.BackColor = System.Drawing.Color.Black;
+			this.pictureBox5.Location = new System.Drawing.Point(3, 35);
+			this.pictureBox5.Name = "pictureBox5";
+			this.pictureBox5.Size = new System.Drawing.Size(30, 27);
+			this.pictureBox5.TabIndex = 4;
+			this.pictureBox5.TabStop = false;
+			this.pictureBox5.Click += new System.EventHandler(this.pictureBox1_Click);
+			// 
+			// pictureBox4
+			// 
+			this.pictureBox4.BackColor = System.Drawing.Color.Fuchsia;
+			this.pictureBox4.Location = new System.Drawing.Point(111, 3);
+			this.pictureBox4.Name = "pictureBox4";
+			this.pictureBox4.Size = new System.Drawing.Size(30, 29);
+			this.pictureBox4.TabIndex = 3;
+			this.pictureBox4.TabStop = false;
+			this.pictureBox4.Click += new System.EventHandler(this.pictureBox1_Click);
+			// 
+			// pictureBox3
+			// 
+			this.pictureBox3.BackColor = System.Drawing.Color.Blue;
+			this.pictureBox3.Location = new System.Drawing.Point(75, 3);
+			this.pictureBox3.Name = "pictureBox3";
+			this.pictureBox3.Size = new System.Drawing.Size(30, 29);
+			this.pictureBox3.TabIndex = 2;
+			this.pictureBox3.TabStop = false;
+			this.pictureBox3.Click += new System.EventHandler(this.pictureBox1_Click);
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.BackColor = System.Drawing.Color.Lime;
+			this.pictureBox2.Location = new System.Drawing.Point(39, 3);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(30, 29);
+			this.pictureBox2.TabIndex = 1;
+			this.pictureBox2.TabStop = false;
+			this.pictureBox2.Click += new System.EventHandler(this.pictureBox1_Click);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackColor = System.Drawing.Color.Red;
+			this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(30, 29);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+			this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(406, 7);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(100, 27);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Text Size";
+			// 
+			// Clearbtn
+			// 
+			this.Clearbtn.BackColor = System.Drawing.Color.Gray;
+			this.Clearbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Clearbtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+			this.Clearbtn.Location = new System.Drawing.Point(552, 7);
+			this.Clearbtn.Name = "Clearbtn";
+			this.Clearbtn.Size = new System.Drawing.Size(75, 60);
+			this.Clearbtn.TabIndex = 6;
+			this.Clearbtn.Text = "Clear";
+			this.Clearbtn.UseVisualStyleBackColor = false;
+			this.Clearbtn.Click += new System.EventHandler(this.Clearbtn_Click);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.BackColor = System.Drawing.Color.Silver;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.ForeColor = System.Drawing.Color.Black;
+			this.label3.Location = new System.Drawing.Point(876, 14);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(116, 20);
+			this.label3.TabIndex = 7;
+			this.label3.Text = "Current Color";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			this.label4.Font = new System.Drawing.Font("Monotype Corsiva", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+			this.label4.Location = new System.Drawing.Point(-5, 7);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(331, 39);
+			this.label4.TabIndex = 9;
+			this.label4.Text = "Pick a Color and DRAW!";
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(411, 36);
+			this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(92, 21);
+			this.comboBox1.TabIndex = 10;
+			this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+			// 
+			// pictureBox9
+			// 
+			this.pictureBox9.Location = new System.Drawing.Point(1004, 2);
+			this.pictureBox9.Name = "pictureBox9";
+			this.pictureBox9.Size = new System.Drawing.Size(59, 65);
+			this.pictureBox9.TabIndex = 8;
+			this.pictureBox9.TabStop = false;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.BackColor = System.Drawing.Color.Silver;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.ForeColor = System.Drawing.Color.Black;
+			this.label2.Location = new System.Drawing.Point(663, 2);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(62, 20);
+			this.label2.TabIndex = 8;
+			this.label2.Text = "Eraser";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+			this.ClientSize = new System.Drawing.Size(1263, 641);
+			this.Controls.Add(this.pictureBox10);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.pictureBox9);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.Clearbtn);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.panel1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.Name = "Form1";
+			this.ShowIcon = false;
+			this.Text = "Painter";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+			this.panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.PictureBox pictureBox8;
+		private System.Windows.Forms.PictureBox pictureBox7;
+		private System.Windows.Forms.PictureBox pictureBox6;
+		private System.Windows.Forms.PictureBox pictureBox5;
+		private System.Windows.Forms.PictureBox pictureBox4;
+		private System.Windows.Forms.PictureBox pictureBox3;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button Clearbtn;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.PictureBox pictureBox9;
+		private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.PictureBox pictureBox10;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.PictureBox pictureBox11;
+		private System.Windows.Forms.PictureBox pictureBox12;
+	}
+}
---
+++ Paint1/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Paint1
+{
+	public partial class Form1 : Form
+	{
+		Graphics g;
+		int x = -1, y = -1;
+		bool moving = false;
+		Pen pen;
+		
+		public Form1()
+		{
+			InitializeComponent();
+			g = panel1.CreateGraphics();
+			g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+			pen = new Pen(Color.Black, 5);
+			pictureBox9.BackColor = pen.Color;
+			pen.StartCap = pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+			for (int i = 5; i <= 100; i+=5)
+			{
+				comboBox1.Items.Add(i);
+			}
+			comboBox1.Text = "5";
+		}
+
+		private void pictureBox1_Click(object sender, EventArgs e)
+		{
+			PictureBox p = (PictureBox)sender;
+			pen.Color = p.BackColor;
+			pictureBox9.BackColor = p.BackColor;
+		}
+		private void panel1_MouseDown(object sender, MouseEventArgs e)
+		{
+			moving = true;
+			x = e.X;
+			y = e.Y;
+			panel1.Cursor = Cursors.Cross;
+		}
+
+		private void panel1_MouseMove(object sender, MouseEventArgs e)
+		{
+			if(moving && x != -1 && y != -1)
+			{
+				g.DrawLine(pen, new Point(x, y), e.Location);
+				x = e.X;
+				y = e.Y;
+			}
+		}
+
+		private void Clearbtn_Click(object sender, EventArgs e)
+		{
+			g.Clear(Color.White);
+		}
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+			try
+			{
+				pen.Width = Convert.ToInt32(comboBox1.Text);
+				if (pen.Width < 0)
+				{
+					pen.Width = 5;
+					comboBox1.Text = "5";
+				}
+			}
+			catch (Exception)
+			{
+				pen.Width = 5;
+				comboBox1.Text = "5";
+			}
+		}
+
+		private void pictureBox10_Click(object sender, EventArgs e)
+		{
+			pen.Color = Color.White;
+			pictureBox9.BackColor = Color.White;
+		}
+
+		private void panel1_MouseUp(object sender, MouseEventArgs e)
+		{
+			moving = false;
+			x = -1;
+			y = -1;
+			panel1.Cursor = Cursors.Arrow;
+		}
+	}
+}
